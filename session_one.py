414a11592883f64969a0afed4646a68476df23a8
oluyalireuben <oluyalireuben1@gmail.com>
2020-04-22 10:50:56 +0300

    first commit

---
+++ session_one.py
@@ -0,0 +1,90 @@
+# getting started
+print("Hello, World!")
+
+# syntax
+if 5 > 2:
+    print("Five is greater than two")
+
+# Variables
+x = 23
+y = 45
+print(x + y)
+
+a = "python "
+b = "is "
+c = "awesome"
+print(a + b + c)
+
+u = "I like "
+j = "Codding "
+k = "with python language"
+print(u + j + k)
+
+# python numbers and strings
+h = "Welcome to eMobilis College"
+s = 2
+d = 2.8
+y = 3j
+print(type(s))
+print(type(d))
+print(type(y))
+# Counting of the characters in a statement
+print(len(h))
+# To print all the characters on lower case and upper case
+print(h.lower())
+print(h.upper())
+
+# Working with Operators
+peter = 23 + 23
+jack = 46
+print(jack is peter)
+
+o = ["Ian", "Dylan","Andrew", "Kevin"]
+print("Paul" in o)
+
+
+thistuple = tuple(("apple", "banana", "cherry"  ))
+print(len(thistuple))
+
+# Conditional statements
+zulu = 45
+south = 45
+if zulu < south:print("zulu is less than south")
+elif zulu == south:
+  print("zulu and south are equal")
+else:print("south is greater than zulu")
+
+#  While loops (While loops and for loops)
+peter = 1
+while peter < 23:
+    print(peter)
+    peter +=1
+
+
+jack < 5
+while jack < 5:
+    jack +=1
+    print(jack)
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+# for loop
+noise_makers = ["Ian","Zeff" , "Paul" , "Peter" , "Reuben" , "Kevin" , "Chairman" , "Temmy"]
+for n in noise_makers:
+    print(n)
+# done to print a lis tof decimal numbers
+for s in range(4):
+    print(s)
+
+
+
+
+
+
+
+
